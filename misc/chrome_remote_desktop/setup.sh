fc92fa40df8566acf0e0cce9eda1194f2dd041f5
heethesh <heethesh@gmail.com>
2018-04-14 10:59:25 +0530

    Bash script for fixing chrome remote desktop

---
+++ misc/chrome_remote_desktop/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/opt/google/chrome-remote-desktop/chrome-remote-desktop --stop
+sudo cp chrome-remote-desktop /opt/google/chrome-remote-desktop/chrome-remote-desktop
+/opt/google/chrome-remote-desktop/chrome-remote-desktop --start
