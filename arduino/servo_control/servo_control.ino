63b64102d905018af02d02a5e5fe8c1c4a3821ff
heethesh <heethesh@gmail.com>
2018-05-01 12:21:42 +0530

    Added servo driver library dep

--- arduino/servo_control/servo_control.ino
+++ arduino/servo_control/servo_control.ino
@@ -1,6 +1,6 @@
 //********** Libraries **********//
 #include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
+#include "Adafruit_PWMServoDriver.h"
 
 //********** Macros **********//
 #define NUM_SERVOS  3
